5feba3553be1d8514879b94ac7fef7dbd53eb15c
Andriy Chapran <chapran66@gmail.com>
2018-04-20 18:47:37 +0300

    refactored contactsList reducer, added object spread support

--- src/js/modules/ContactsList/reducer.js
+++ src/js/modules/ContactsList/reducer.js
@@ -17,52 +17,42 @@ const defaultState = {
 export default function (state = defaultState, action) {
   switch (action.type) {
     case REQUEST_CONTACTS:
-      return Object.assign({}, state, { isFetching: true })
+      return { ...state, isFetching: true }
     case SHOW_ERROR:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         fetchFailed: true
-      })
+      }
     case LOAD_CONTACTS:
       action.data.forEach(item => {
         item.dateOfBirth = new Date(item.dateOfBirth)
       });
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         contactsList: action.data
-      })
+      }
     case TOGGLE_FAVORITE:
-      const changedContactIdx = state.contactsList.findIndex(item => item.id === action.id);
-      const contactsList = [
-        ...state.contactsList.slice(0, changedContactIdx),
-        Object.assign(
-          {},
-          state.contactsList[changedContactIdx],
-          { favorite: !state.contactsList[changedContactIdx].favorite }
-        ),
-        ...state.contactsList.slice(changedContactIdx + 1, state.contactsList.length)
-      ];
-      return Object.assign({}, state, { contactsList })
-    case DELETE_CONTACT:
-      let contactIdx = state.contactsList.findIndex(item => item.id === action.id);
-      return Object.assign({}, state, {
-        contactsList: [
-          ...state.contactsList.slice(0, contactIdx),
-          ...state.contactsList.slice(contactIdx + 1, state.contactsList.length)
-        ]
+      const contactsList = state.contactsList.map(item => {
+        return item.id === action.id ? { ...item, favorite: !item.favorite } : item
       })
+      return { ...state, contactsList }
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contactsList: state.contactsList.filter(item => item.id !== action.id),
+      }
     case ADD_CONTACT:
       action.data.id = Date.now();
-      return Object.assign({}, state, { contactsList: [...state.contactsList, action.data] })
+      return { ...state, contactsList: [...state.contactsList, action.data] }
     case EDIT_CONTACT:
-      contactIdx = state.contactsList.findIndex(item => item.id == action.id);
-      return Object.assign({}, state, {
-        contactsList: [
-          ...state.contactsList.slice(0, contactIdx),
-          Object.assign({}, action.data, { id: action.id }),
-          ...state.contactsList.slice(contactIdx + 1, state.contactsList.length)
-        ]
-      })
+      return {
+        ...state,
+        contactsList: state.contactsList.map(item => {
+          return item.id === action.id ? { ...action.data, id: action.id } : item
+        })
+      }
     default:
       return state;
   }
